7799218b91eebd8f62d549ddb0a620d21b552f2d
kevinasadi <kevin.asadi@elev.ga.ntig.se>
2024-12-03 11:13:08 +0100

    Create arduinocode.ino

---
+++ arduinocode/arduinocode.ino
@@ -0,0 +1,17 @@
+#include "U8glib.h"
+#include <RTClib.h>
+#include <Wire.h>
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
+
+RTC_DS3231 rtc;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
